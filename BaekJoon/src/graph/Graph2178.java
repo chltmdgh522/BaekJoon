f753bb1eaed9c89a84fca3bf53f48f0a5d0e50c9
seung_ho_choi.s <chltmdgh517@naver.com>
2023-08-19 19:41:44 +0900

    보안 2178

--- BaekJoon/src/graph/Graph2178.java
+++ BaekJoon/src/graph/Graph2178.java
@@ -1,5 +1,8 @@
 package graph;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.*;
 
 public class Graph2178 {
@@ -12,56 +15,49 @@ public class Graph2178 {
     static int[] dy = {0, 0, -1, 1};
 
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        n = sc.nextInt();
-        m = sc.nextInt();
+    public static void main(String[] args) throws IOException {
+        BufferedReader bfr=new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer stk = new StringTokenizer(bfr.readLine());
+        n = Integer.parseInt(stk.nextToken());
+        m = Integer.parseInt(stk.nextToken());
+
 
         arr = new int[n][m];
         visited = new boolean[n][m];
 
-        for(int i=0;i<n;i++) {
-            String input = sc.next();
-            for(int j=0;j<m;j++) {
-                arr[i][j] = input.charAt(j) - '0'; //int 형 쌉가능
+        for(int i=0; i<n; i++){
+            String input=stk.nextToken();
+            for(int j=0; j<m; j++){
+                arr[i][j]=input.charAt(j)-'0';
             }
         }
 
-        //(0, 0) -> (n-1, m-1)
-        //최단거리 -> bfs 사용
-        visited[0][0] = true; //출발지
-        bfs(0, 0);
+        visited[0][0]=true;
+        bfs(0,0);
         System.out.println(arr[n-1][m-1]);
-
-        /*for(int i=0;i<n;i++) {
-            for(int j=0;j<m;j++) {
-                System.out.print(arr[i][j]+" ");
-            }
-            System.out.println();
-        }*/
 
     }
 
     public static void bfs(int x, int y) {
-        Queue<int[]> queue=new LinkedList<>();
-        queue.add(new int[]{x,y});
+       Queue<int[]> queue=new LinkedList<>();
+       queue.add(new int[]{x,y});
 
-        while(!queue.isEmpty()){
-            int[] now=queue.poll();
-            int nx=now[0];
-            int ny=now[1];
+       while(!queue.isEmpty()){
+           int[] temp = queue.poll();
+           int nx=temp[0];
+           int ny=temp[1];
 
-            for(int i=0; i<4; i++){
-                int nextX=nx+dx[i];
-                int nextY=ny+dy[i];
-                if(nextX>=0&&nextX<n&&nextY>=0&&nextY<m){
-                    if(arr[nextX][nextY]==1&&!visited[nextX][nextY]){
-                        visited[nextX][nextY]=true;
-                        queue.add(new int[]{nextX,nextY});
-                        arr[nextX][nextY]=arr[nx][ny]+1;
-                    }
-                }
-            }
-        }
+           for(int i=0; i<4; i++){
+               int nextDx= nx+dx[i];
+               int nextDy=ny+dy[i];
+               if(nextDx>=0&&nextDy>=0&&nextDy<m&&nextDx<n){
+                   if(!visited[nextDx][nextDy]&&arr[nextDx][nextDy]==1){
+                       queue.add(new int[]{nextDx,nextDy});
+                       visited[nextDx][nextDy]=true;
+                       arr[nextDx][nextDy]=arr[nx][ny]+1;
+                   }
+               }
+           }
+       }
     }
 }

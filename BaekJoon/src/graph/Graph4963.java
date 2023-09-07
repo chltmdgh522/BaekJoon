c9550dbd56f6facb917ec53dce03b6ab0c25ec9e
seung_ho_choi.s <chltmdgh517@naver.com>
2023-09-07 14:18:01 +0900

    Greedy 4963

--- BaekJoon/src/graph/Graph4963.java
+++ BaekJoon/src/graph/Graph4963.java
@@ -1,5 +1,8 @@
 package graph;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.*;
 
 public class Graph4963 {
@@ -12,64 +15,63 @@ public class Graph4963 {
     static int[] dx = {1, -1, 0, 0, 1, 1, -1, -1};
     static int[] dy = {0, 0, 1, -1, 1, -1, 1, -1};
 
-    public static void main(String[] args) {
-        List<Integer> list=new ArrayList<>();
-        int count=0;
-        int fcnt=0;
-        n = 1;
-        m = 1;
-        while (n != 0 && m != 0) {
-            count=0;
-            Scanner sc = new Scanner(System.in);
-            n = sc.nextInt();
-            m = sc.nextInt();
+    static int count;
 
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        while (true) {
+            st = new StringTokenizer(br.readLine());
+            n = Integer.parseInt(st.nextToken());
+            m = Integer.parseInt(st.nextToken());
+            int temp = 0;
+            temp = n;
+            n = m;
+            m = temp;
+            if (n == 0 && m == 0) {
+                break;
+            }
             arr = new int[n][m];
             visited = new boolean[n][m];
             for (int i = 0; i < n; i++) {
+                st = new StringTokenizer(br.readLine());
                 for (int j = 0; j < m; j++) {
-                    arr[i][j] = sc.nextInt();
+                    arr[i][j] = Integer.parseInt(st.nextToken());
                 }
             }
-            for(int i=0; i<n; i++){
-                for(int j=0; j<m; j++){
-                    if(arr[i][j]==1&&!visited[i][j]){
-                        visited[i][j]=true;
-                        fcnt +=bfs(i, j);
+            count = 0;
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < m; j++) {
+                    if (arr[i][j] == 1 && !visited[i][j]) {
+                        visited[i][j] = true;
+                        bfs(i,j);
                         count++;
                     }
                 }
             }
 
-            if(n!=0&&m!=0) {
-                list.add(fcnt);
-
-            }
+            System.out.println(count);
         }
-        for (int c : list) {
-            System.out.println(c);
-        }
-
     }
 
-    public static int bfs(int i, int j){
-        int cnt=0;
+    public static int bfs(int i, int j) {
+        int cnt = 0;
         cnt++;
-        Queue<int[]> queue=new LinkedList<>();
-        queue.add(new int[]{i,j});
+        Queue<int[]> queue = new LinkedList<>();
+        queue.add(new int[]{i, j});
 
-        while(!queue.isEmpty()){
+        while (!queue.isEmpty()) {
             int[] poll = queue.poll();
-            int staticX=poll[0];
-            int staticY=poll[1];
+            int staticX = poll[0];
+            int staticY = poll[1];
 
-            for(int k=0; k<8; k++){
-                int nextStaticX=staticX+dx[k];
-                int nextStaticY=staticY+dy[k];
-                if(nextStaticX>=0&&nextStaticX<n&&nextStaticY>=0&&nextStaticY<m){
-                    if(arr[nextStaticX][nextStaticY]==1&&!visited[nextStaticX][nextStaticY]){
-                        visited[nextStaticX][nextStaticY]=true;
-                        queue.add(new int[]{nextStaticX,nextStaticY});
+            for (int k = 0; k < 8; k++) {
+                int nextStaticX = staticX + dx[k];
+                int nextStaticY = staticY + dy[k];
+                if (nextStaticX >= 0 && nextStaticX < n && nextStaticY >= 0 && nextStaticY < m) {
+                    if (arr[nextStaticX][nextStaticY] == 1 && !visited[nextStaticX][nextStaticY]) {
+                        visited[nextStaticX][nextStaticY] = true;
+                        queue.add(new int[]{nextStaticX, nextStaticY});
 
                     }
                 }

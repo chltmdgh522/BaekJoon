857d80608a85b61b885a2e3e6c5cf318f06d5e5d
seung_ho_choi.s <chltmdgh517@naver.com>
2023-09-09 21:50:50 +0900

    Greedy 10026

--- BaekJoon/src/graph/Graph10026.java
+++ BaekJoon/src/graph/Graph10026.java
@@ -7,54 +7,104 @@ import java.util.Scanner;
 public class Graph10026 {
     static int n;
 
-    static char [][]arr;
-    static boolean [][]visited;
+    static char[][] arr;
+    static boolean[][] visited;
 
-    static int [] dx={-1,1,0,0};
-    static int [] dy={0,0,-1,1};
+    static int[] dx = {-1, 1, 0, 0};
+    static int[] dy = {0, 0, -1, 1};
+
+    static int count = 0;
+
 
     public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
+        char identify;
+        Scanner sc = new Scanner(System.in);
 
-        n=sc.nextInt();
+        n = sc.nextInt();
 
-        arr=new char[n][n];
-        visited=new boolean[n][n];
+        arr = new char[n][n];
+        visited = new boolean[n][n];
 
-        for(int i=0; i<n; i++){
-            String input=sc.next();
-            for(int j=0; j<n; j++){
-                char c = input.charAt(j);
+        for (int i = 0; i < n; i++) {
+            String input = sc.next();
+            for (int j = 0; j < n; j++) {
+                arr[i][j] = input.charAt(j);
             }
         }
 
-        for(int i=0; i<n; i++){
-            for(int j=0; j<n; j++){
-                if(!visited[i][j]&&arr[i][j]=='R')
-                visited[i][j]=true;
-                bfs(i,j);
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (!visited[i][j]) {
+                    visited[i][j] = true;
+                    switch (arr[i][j]) {
+                        case 'R':
+                            System.out.println("R");
+                            identify = 'R';
+                            bfs(i, j, identify);
+                            count++;
+                            System.out.println("1");
+                            break;
+                        case 'B':
+                            System.out.println("B");
+                            identify = 'B';
+                            bfs(i, j, identify);
+                            count++;
+                            System.out.println("2");
+                            break;
+                        case 'G':
+                            System.out.println("G");
+                            identify = 'G';
+                            bfs(i, j, identify);
+                            count++;
+                            System.out.println("3");
+                            break;
+                    }
+                }
             }
         }
+
+
+        System.out.println(count);
 
 
     }
 
-    static void bfs(int i, int j){
-        Queue<int[]> queue= new LinkedList<>();
-        queue.add(new int[]{i,j});
+    static void bfs(int i, int j, char id) {
+        Queue<int[]> queue = new LinkedList<>();
+        queue.add(new int[]{i, j});
 
-        while(!queue.isEmpty()){
+
+        while (!queue.isEmpty()) {
             int[] poll = queue.poll();
-            int staticX=poll[0];
-            int staticY=poll[1];
-            if(staticX>=0&&staticX<n&&staticY>=0&&staticY<n){
-                for(int k=0; k<4; k++){
-                    int staticNextX=staticX+dx[k];
-                    int staticNextY=staticY+dy[k];
-                    if(staticNextX>=0&&staticNextX<n&&staticNextY>=0&&staticNextY<n){
-                        if(arr[staticNextX][staticNextY]=='R'&&!visited[staticNextX][staticNextY]){
-                            queue.add(new int[]{staticNextX,staticNextY});
-                            visited[staticNextX][staticNextY]=true;
+            int staticX = poll[0];
+            int staticY = poll[1];
+            if (staticX >= 0 && staticX < n && staticY >= 0 && staticY < n) {
+                for (int k = 0; k < 4; k++) {
+                    int staticNextX = staticX + dx[k];
+                    int staticNextY = staticY + dy[k];
+                    if (id == arr[staticNextX][staticNextY]) {
+                        if (staticNextX >= 0 && staticNextX < n && staticNextY >= 0 && staticNextY < n) {
+                            if (!visited[staticNextX][staticNextY]) {
+                                switch (arr[staticNextX][staticNextY]) {
+                                    case 'R':
+                                        queue.add(new int[]{staticNextX, staticNextY});
+
+                                        visited[staticNextX][staticNextY] = true;
+                                        System.out.println("a");
+                                        break;
+                                    case 'G':
+                                        queue.add(new int[]{staticNextX, staticNextY});
+                                        visited[staticNextX][staticNextY] = true;
+                                        System.out.println("b");
+                                        break;
+                                    case 'B':
+                                        queue.add(new int[]{staticNextX, staticNextY});
+                                        visited[staticNextX][staticNextY] = true;
+                                        System.out.println("c");
+                                        break;
+                                }
+
+                            }
                         }
                     }
                 }
@@ -63,3 +113,9 @@ public class Graph10026 {
 
     }
 }
+  /*
+  *
+  *   if (arr[staticNextX][staticNextY] == 'R' || arr[staticNextX][staticNextY] == 'B' || arr[staticNextX][staticNextY] == 'G') {
+                                queue.add(new int[]{staticNextX, staticNextY});
+                                visited[staticNextX][staticNextY] = true;
+                            }*/

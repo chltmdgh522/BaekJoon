2747d6a37cf322c8cee0899e820101b9e7b2f983
seung_ho_choi.s <chltmdgh517@naver.com>
2023-10-27 10:40:19 +0900

    Greedy1213

---
+++ BaekJoon/src/greedy/Greedy1213.java
@@ -0,0 +1,16 @@
+package greedy;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+public class Greedy1213 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String sentence = String.valueOf(br.readLine());
+
+    }
+}

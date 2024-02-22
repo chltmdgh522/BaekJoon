e8130a57794fc3c0e55bf9139e2e246a96f0b3e4
seung_ho_choi.s <chltmdgh517@naver.com>
2024-02-22 22:38:13 +0900

    Graph 11403 완료

---
+++ BaekJoon/src/graph/Graph11403.java
@@ -0,0 +1,50 @@
+package graph;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Graph11403 {
+    static int n;
+
+    static int[][] arr;
+
+    static BufferedWriter bw;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st;
+        n = Integer.parseInt(br.readLine());
+        arr = new int[n][n];
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < n; j++) {
+                arr[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+        solution();
+        bw.flush();
+        bw.close();
+    }
+
+    private static void solution() throws IOException {
+        for (int k = 0; k < n; k++) {
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < n; j++) {
+                    if (arr[i][k] == 1 && arr[k][j] == 1) {
+                        arr[i][j] = 1;
+                    }
+                }
+            }
+        }
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                bw.write(arr[i][j] + " ");
+            }
+            bw.write("\n");
+        }
+    }
+
+
+}

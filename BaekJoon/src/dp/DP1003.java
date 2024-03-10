fea6c7a8616582106e275dfee22ab454bf10356a
seung_ho_choi.s <chltmdgh517@naver.com>
2024-03-11 00:18:41 +0900

    DP1003

--- BaekJoon/src/dp/DP1003.java
+++ BaekJoon/src/dp/DP1003.java
@@ -3,42 +3,33 @@ package dp;
 import java.io.*;
 
 public class DP1003 {
-    static int zcnt;
-
-    static int ocnt;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         int t = Integer.parseInt(br.readLine());
-        int[] num = new int[t];
+
         int[] dp = new int[41];
 
         dp[0] = 0;
         dp[1] = 1;
 
-        for (int i = 0; i < t; i++) {
-            num[i] = Integer.parseInt(br.readLine());
-            fibonacci(num[i]);
-            bw.write(zcnt + " " + ocnt);
-            bw.write("\n");
-            zcnt=ocnt=0;
-
+        for (int i = 2; i < 41; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2];
         }
 
+        for (int i = 0; i < t; i++) {
+            int n = Integer.parseInt(br.readLine());
+            if(n==0){
+                bw.write("1 0");
+                bw.write("\n");
+            }else{
+            bw.write(dp[n-1] + " " + dp[n]);
+            bw.write("\n");
+                }
+        }
         bw.flush();
         bw.close();
     }
 
-    private static int fibonacci(int n) {
-        if (n == 0) {
-            zcnt++;
-            return 0;
-        } else if (n == 1) {
-            ocnt++;
-            return 1;
-        } else {
-            return fibonacci(n - 1) + fibonacci(n - 2);
-        }
-    }
 }

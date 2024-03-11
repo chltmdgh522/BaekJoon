060e8594318dd8d5a31baff9be2476f95e65eb31
seung_ho_choi.s <chltmdgh517@naver.com>
2024-03-11 21:12:03 +0900

    DP11726 완료

---
+++ BaekJoon/src/dp/DP11726.java
@@ -0,0 +1,21 @@
+package dp;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class DP11726 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int[] dp = new int[1001];
+
+        dp[1] = 1;
+        dp[2] = 2;
+
+        for (int i = 3; i < n + 1; i++) {
+            dp[i] = (dp[i - 1] + dp[i - 2]) % 10007;
+        }
+        System.out.println(dp[n]);
+    }
+}

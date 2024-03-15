fa0f415e6a7eab0727a37383df1757f425ecccdb
seung_ho_choi.s <chltmdgh517@naver.com>
2024-03-16 00:07:48 +0900

    DP1932 완료

--- BaekJoon/src/dp/DP1932.java
+++ BaekJoon/src/dp/DP1932.java
@@ -3,27 +3,31 @@ package dp;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class DP1932 {
     public static void main(String[] args) throws IOException {
-        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
 
-        int n=Integer.parseInt(br.readLine());
-        int []num=new int[n+1];
-        int []dp=new int[n+1];
+        int n = Integer.parseInt(br.readLine());
+        int[] num = new int[n + 1];
+        int[][] dp = new int[n + 1][n + 1];
 
 
-        for(int i=1; i<n+1; i++){
-            st=new StringTokenizer(br.readLine());
-            for(int j=1; j<i+1; j++){
-                num[j]=Integer.parseInt(st.nextToken());
-
-                dp[i]=Math.max(dp[i-1]+num[j-1], dp[i-1]+num[j]);
-
-                dp[i-1]=Math.max(dp[i-1]+num[j-1], dp[i-1]+num[j]);
+        for (int i = 1; i < n + 1; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 1; j < i + 1; j++) {
+                num[j] = Integer.parseInt(st.nextToken());
+                if (i == 1) {
+                    dp[1][1] = num[j];
+                } else {
+                    dp[i][j] = Math.max(dp[i - 1][j - 1] + num[j], dp[i - 1][j] + num[j]);
+                }
             }
         }
+        Arrays.sort(dp[n]);
+        System.out.println(dp[n][n]);
     }
 }

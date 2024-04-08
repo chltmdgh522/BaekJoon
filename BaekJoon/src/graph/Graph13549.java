a827da4de35de45249a290aaeddbfc1e0d1a8124
seung_ho_choi.s <chltmdgh517@naver.com>
2024-04-08 17:27:09 +0900

    Graph 13549 완료

--- BaekJoon/src/graph/Graph13549.java
+++ BaekJoon/src/graph/Graph13549.java
@@ -3,66 +3,50 @@ package graph;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Graph13549 {
     static int n;
     static int k;
-    static int cnt = 0;
-    static int min = Integer.MAX_VALUE;
+    static boolean[] visit = new boolean[100001];
+    static int[] dx = {-1, 1};
 
-    static char[] sign;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         n = Integer.parseInt(st.nextToken());
         k = Integer.parseInt(st.nextToken());
-        sign=new char[3];
 
-        sign[0]='+';
-        sign[1]='-';
-        sign[2]='*';
-
-        dfs(n);
-        System.out.println(min);
+        bfs();
     }
 
-    private static int dfs(int num) {
+    private static void bfs() {
+        Queue<int[]> queue = new LinkedList<>();
+        queue.offer(new int[]{n, 0});
 
-        if (num == k) {
-            min=Math.min(min,cnt);
-            return 1;
+        while (!queue.isEmpty()) {
+            int[] poll = queue.poll();
+            int pos = poll[0];
+            int time = poll[1];
+
+            if (pos == k) {
+                System.out.println(time);
+                return;
+            }
+
+            if (pos * 2 <= 100000 && !visit[pos * 2] ) {
+                visit[pos * 2] = true;
+                queue.offer(new int[]{pos * 2, time});
+            }
+            for (int i = 0; i < 2; i++) {
+                if (pos + dx[i] <= 100000 && pos + dx[i] >= 0 && !visit[pos + dx[i]]) {
+                    visit[pos + dx[i]] = true;
+                    queue.offer(new int[]{pos + dx[i], time + 1});
+                }
+            }
         }
-
-
-        return 1;
-
     }
 
 }
-
-/*
-* private static int dfs(int num) {
-
-        if (num == k) {
-            min=Math.min(min,cnt);
-            return 1;
-        }
-        if (num > k) {
-            cnt++;
-            num -= 1;
-            dfs(num);
-        } else {
-            int temp = k % num;
-            if (k / n > 1 || temp > n / 2) {
-                num *= 2;
-                dfs(num);
-            } else {
-                num += 1;
-                cnt++;
-                dfs(num);
-            }
-        }
-        return 1;
-
-    }*/

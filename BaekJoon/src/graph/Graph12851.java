6552b474f778f9837a7c7f71f895240d89de2651
seung_ho_choi.s <chltmdgh517@naver.com>
2024-04-10 23:46:01 +0900

    Graph 12851 완료

--- BaekJoon/src/graph/Graph12851.java
+++ BaekJoon/src/graph/Graph12851.java
@@ -3,17 +3,17 @@ package graph;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Graph12851 {
 
     static int n;
     static int k;
-    static int[] dx = {1, -1};
+    static int[] dx = {1, -1, 0};
 
+    static boolean[] visit = new boolean[100001];
     static int min = Integer.MAX_VALUE;
+
     static int cnt = 0;
 
     public static void main(String[] args) throws IOException {
@@ -23,40 +23,50 @@ public class Graph12851 {
         n = Integer.parseInt(st.nextToken());
         k = Integer.parseInt(st.nextToken());
 
+        visit[n] = true;
         bfs();
         System.out.println(min);
         System.out.println(cnt);
     }
 
     private static void bfs() {
-        Queue<int[]> queue = new LinkedList<>();
-        queue.add(new int[]{n, 0});
-
-        while (!queue.isEmpty()) {
-            int[] poll = queue.poll();
-            int pos = poll[0];
-            int time = poll[1];
-
-            if (pos == k) {
-                if (cnt == 0) {
-                    min = time;
-                }
-                if (min == time) {
-                    cnt++;
-                }else{
-                    return;
-                }
-
+        Queue<Integer> queue = new LinkedList<>();
+        queue.add(n);
+        int time = 0;
+        while (!queue.isEmpty() && time <= min) {
+            if (n == k) {
+                min = 0;
+                cnt = 1;
+                return;
             }
-            if (pos * 2 <= 100000) {
-                queue.add(new int[]{pos * 2, time + 1});
-            }
-            for (int i = 0; i < 2; i++) {
-                if (pos + dx[i] >= 0 && pos + dx[i] <= 100000) {
-                    queue.add(new int[]{pos + dx[i], time + 1});
+            Set<Integer> set = new LinkedHashSet<>();
+            int len = queue.size();
+            time++;
+            for (int j = 0; j < len; j++) {
+                int pos = queue.poll();
 
+                for (int i = 0; i < 3; i++) {
+                    if (i == 2) {
+                        dx[2] = pos;
+                    }
+                    if (pos + dx[i] >= 0 && pos + dx[i] <= 100000 && !visit[pos + dx[i]]) {
+                        queue.add(pos + dx[i]);
+                        set.add(pos + dx[i]);
+                    }
+                    if (pos + dx[i] == k) {
+                        if (cnt == 0) {
+                            min = time;
+                        }
+                        if (min == time) {
+                            cnt++;
+                        }
+                    }
                 }
             }
+            for (Integer i : set) {
+                visit[i] = true;
+            }
+
         }
     }
 }

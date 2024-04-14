132e23b51f8f21bfebb930d5151e0f146a50c29f
seung_ho_choi.s <chltmdgh517@naver.com>
2024-04-15 00:05:58 +0900

    Implement 14890 미완료

--- BaekJoon/src/implement/Implement14890.java
+++ BaekJoon/src/implement/Implement14890.java
@@ -34,30 +34,6 @@ public class Implement14890 {
     }
 
     private static void imp(){
-        boolean flag=true;
-        for(int i=0; i<n*2; i++){
-            if(i>=n){
-                i-=6;
-                for(int j=0; j<n; j++){
-                    if(i==0) {
-                        temp=arr[j][i];
-                    }else{
-                        if(flag && temp-arr[j][i]==1){
-                            temp=arr[j][i];
-                            flag=false;
-                        }
-                        else if(!flag && temp==arr[j][i]){
-                            
-                        }
-                    }
 
-                }
-            }else{
-                for(int j=0; j<n; j++){
-
-                }
-            }
-
-        }
     }
 }

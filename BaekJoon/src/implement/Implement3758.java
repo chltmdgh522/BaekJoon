8508495892aec2e75e95013837d29904b6600e1b
seung_ho_choi.s <chltmdgh517@naver.com>
2024-05-03 00:33:47 +0900

    Implement3758 완료

--- BaekJoon/src/implement/Implement3758.java
+++ BaekJoon/src/implement/Implement3758.java
@@ -46,11 +46,11 @@ public class Implement3758 {
                 problem = Integer.parseInt(st.nextToken());
                 score = Integer.parseInt(st.nextToken());
 
-                if (totalScore[teamId-1][problem-1] != 0) {
-                    score = Math.max(totalScore[teamId-1][problem-1], score);
+                if (totalScore[teamId - 1][problem - 1] != 0) {
+                    score = Math.max(totalScore[teamId - 1][problem - 1], score);
                 }
-                totalScore[teamId-1][problem-1] = score;
-                cnt[teamId-1] += 1;
+                totalScore[teamId - 1][problem - 1] = score;
+                cnt[teamId - 1] += 1;
                 list.add(teamId);
             }
             rank();
@@ -61,8 +61,8 @@ public class Implement3758 {
     private static void rank() {
 
         int[][] result_score = new int[n][2];
-        for (int i = 0; i < n ; i++) {
-            result_score[i][0]=i+1;
+        for (int i = 0; i < n; i++) {
+            result_score[i][0] = i + 1;
             for (int j = 0; j < k; j++) {
                 result_score[i][1] += totalScore[i][j];
             }
@@ -70,44 +70,53 @@ public class Implement3758 {
 
         Arrays.sort(result_score, Comparator.comparingInt((int[] a) -> a[1]).reversed()); // 이차원 배열 두번째 열 기준으로 정렬
 
-        for (int[] ints : result_score) {
-            System.out.print(ints[0]);
-            System.out.print(ints[1]);
-            System.out.println();
-        }
-        int rank=0;
-        for(int i=0; i<n; i++){
-            if(result_score[i][0]==my){
-                rank=i+1;
+
+
+        int rank = 0;
+        for (int i = 0; i < n; i++) {
+            if (result_score[i][0] == my) {
+                rank = i + 1;
             }
         }
 
+        int equal = result_score[rank - 1][1];
+        int my_cnt = cnt[my - 1];
+        int down_rank = 0;
+        int equal_rank = 0;
 
-        int equal=result_score[rank-1][1];
-        int my_cnt=cnt[my-1];
+        Collections.reverse(list);
+        int f_rank = 0;
+        int m_rank=0;
+        for (int i = 0; i < n; i++) {
+            if (result_score[i][1] > equal){
+                m_rank++;
+            }
 
-        for(int i=0; i<n; i++){
-            if(result_score[i][1]==equal && result_score[i][0]!=my){
 
-                if(my_cnt>cnt[result_score[i][0]-1]){
-                    rank++;
-                }else if (my_cnt<cnt[result_score[i][0]-1]){
-                    rank--;
-                } else{
-                    boolean flag=false; // 값 동등일떄 빨리 나온거
-                    for(int index : list){
+            if (result_score[i][1] == equal && result_score[i][0] != my) {
+                f_rank++;
+                if (my_cnt < cnt[result_score[i][0] - 1]) {
+                    down_rank++;
+                } else if (my_cnt == cnt[result_score[i][0] - 1]) {
+                    boolean flag = false; // 값 동등일떄 빨리 나온거
+                    for (int index : list) {
                         if (index == result_score[i][0]) {
                             flag = true;
                             break;
+                        } else if (index == my) {
+
+                            break;
                         }
                     }
-                    if(flag){
-                        rank++;
+                    if (flag) {
+                        equal_rank++;
                     }
                 }
             }
         }
+        f_rank++; //자기 자신
 
-        System.out.println(rank);
+        f_rank = f_rank - down_rank - equal_rank+m_rank;
+        System.out.println(f_rank);
     }
 }

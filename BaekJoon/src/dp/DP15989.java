181de0aea81803cb826d9d553ed5458b5b5001d5
seung_ho_choi.s <chltmdgh517@naver.com>
2024-05-08 19:09:26 +0900

    DP15989 완료

--- BaekJoon/src/dp/DP15989.java
+++ BaekJoon/src/dp/DP15989.java
@@ -7,7 +7,35 @@ import java.util.Arrays;
 
 public class DP15989 {
 
-    static int n;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n;
+
+        int[][] dp = new int[10001][4];
+        int t = Integer.parseInt(br.readLine());
+        dp[1][1] = 1;
+        dp[2][1] = 1;
+        dp[2][2] = 1;
+        dp[3][1] = 1;
+        dp[3][2] = 1;
+        dp[3][3] = 1;
+
+        for(int i=4; i<10001; i++){
+            dp[i][1]=dp[i-1][1];
+            dp[i][2]=dp[i-2][1]+dp[i-2][2];
+            dp[i][3]=dp[i-3][1]+dp[i-3][2]+dp[i-3][3];
+        }
+        for (int i = 0; i < t; i++) {
+            n = Integer.parseInt(br.readLine());
+            System.out.println(dp[n][1]+dp[n][2]+dp[n][3]);
+        }
+    }
+
+}
+
+/*
+*  static int n;
 
     static int[] dp;
     public static void main(String[] args) throws IOException {
@@ -31,5 +59,4 @@ public class DP15989 {
         }
 
         System.out.println(dp[n]);
-    }
-}
+    }*/

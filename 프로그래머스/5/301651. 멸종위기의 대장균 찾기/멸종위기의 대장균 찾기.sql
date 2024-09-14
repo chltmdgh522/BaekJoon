52767668b1633af56beac1eb3a0a5bee6e3a7d6a
seung_ho_choi.s <74850409+chltmdgh522@users.noreply.github.com>
2024-09-15 01:20:13 +0900

    [level 5] Title: 멸종위기의 대장균 찾기, Time: , Memory: undefined -BaekjoonHub

---
+++ 프로그래머스/5/301651. 멸종위기의 대장균 찾기/멸종위기의 대장균 찾기.sql
@@ -0,0 +1,23 @@
+WITH RECURSIVE CTE AS (
+    -- 최상위 부모에 대한 정의
+    SELECT 
+        id, 
+        parent_id, 
+        1 AS lvl
+    FROM ecoli_data
+    WHERE parent_id IS NULL    
+    UNION ALL    
+    -- 자식에 대한 정의 (부모와 관계)
+    SELECT 
+        c.id, 
+        c.parent_id, 
+        p.lvl + 1
+    FROM CTE p JOIN ecoli_data c ON c.parent_id = p.id
+)
+SELECT 
+    COUNT(*) AS COUNT, 
+    lvl AS GENERATION
+FROM CTE a
+WHERE NOT EXISTS (SELECT 1 FROM ecoli_data b WHERE b.parent_id = a.id)
+GROUP BY 2
+ORDER BY 2;

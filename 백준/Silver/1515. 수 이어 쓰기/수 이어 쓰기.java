e0fe401859c60727eb982229a20b8be4370c3eac
seung_ho_choi.s <74850409+chltmdgh522@users.noreply.github.com>
2024-11-19 10:40:57 +0900

    [Silver III] Title: 수 이어 쓰기, Time: 136 ms, Memory: 16380 KB -BaekjoonHub

---
+++ 백준/Silver/1515. 수 이어 쓰기/수 이어 쓰기.java
@@ -0,0 +1,25 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+        String text=br.readLine();
+        int base=0;
+        int point=0;
+        while(base++<=30000){
+            String temp=String.valueOf(base);
+            for(int i=0; i<temp.length(); i++){
+                if(text.charAt(point)==temp.charAt(i)){
+                    point++;
+                }
+                if(point==text.length()){
+                    System.out.println(base);
+                    return;
+                }
+            }
+        }
+
+    }
+}

e812814bba43b449aca0dd2ac659b600b97a3dc6
seung_ho_choi.s <74850409+chltmdgh522@users.noreply.github.com>
2024-12-12 11:38:30 +0900

    [Silver II] Title: 예산, Time: 140 ms, Memory: 15244 KB -BaekjoonHub

---
+++ 백준/Silver/2512. 예산/예산.java
@@ -0,0 +1,46 @@
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+        int left=0;
+        int right=-1;
+        int []num=new int[N];
+        StringTokenizer st=new StringTokenizer(br.readLine());
+        for(int i=0; i<N; i++){
+            num[i]=Integer.parseInt(st.nextToken());
+            right=Math.max(right,num[i]);
+        }
+        int m=Integer.parseInt(br.readLine());
+        while(left<=right){
+            int mid=(left+right)/2;
+            long budget=0;
+
+            for(int i=0; i<num.length; i++){
+                if(num[i]>mid){
+                    budget+=mid;
+                }else{
+                    budget+=num[i];
+                }
+            }
+
+            if(budget <=m){
+                left=mid+1;
+            }else{
+                right=mid-1;
+            }
+        }
+
+        System.out.println(right);
+
+    }
+}
